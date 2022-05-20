89f71b92a670211d3aa750c3e65c9a65ea62c295
Joel Kågemyr <kagemyrjoel@gmail.com>
2022-05-20 15:18:32 +0200

    Added some models

--- mutually_backend/api/models.py
+++ mutually_backend/api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=64)
+    birthday = models.DateField()
+
+
+class Chat(models.Model):
+    user1 = models.ForeignKey(User, on_delete=models.CASCADE)
+    user2 = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Message(models.Model):
+    text = models.TextField(max_length=512)
+    sender = models.BooleanField()
+    chat = models.ForeignKey(Chat)
+    time_sent = models.DateTimeField()
+    time_read = models.DateTimeField()
+
+
+
+
+
